7c21aec4640cbc5f500c909e4caafb0acc5fc5bd
Abdulalim Elmozogi <abddo.55242@gmail.com>
2021-06-08 22:03:23 +0200

    stateful widget and random number generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dice'),
+          title: Center(
+            child: Text('Dice'),
+          ),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +18,46 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice  =  1;
+  int rightDice  =  2;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    leftDice = Random().nextInt(6) + 1;
+                  });
+                },
+                child: Image.asset('images/dice$leftDice.png'),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    rightDice = Random().nextInt(6) + 1;
+                  });
+                },
+                child: Image.asset('images/dice$rightDice.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
